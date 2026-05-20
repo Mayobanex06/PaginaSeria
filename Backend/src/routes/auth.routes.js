f2fa5a4a75b11ba33ec98497c485146ca319d5eb
Mayobanex06 <alfonsomayobanex01@hotmail.com>
2026-05-19 22:33:57 -0400

    Agregar limite de intentos al login

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const { loginLimiter } = require("../middlewares/rateLimit.middleware");
 
 const authController = require("../controllers/auth.controller");
 
 module.exports = (sessions, authMiddleware) => {
   router.post("/register", authController.register);
-  router.post("/login", authController.login(sessions));
+  router.post("/login", loginLimiter, authController.login(sessions));
   router.get("/me", authMiddleware, authController.me);
   router.post("/logout", authController.logout(sessions));
 
