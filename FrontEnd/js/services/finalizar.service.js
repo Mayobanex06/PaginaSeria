dff2f4e7a99d4b93c752233d7653c5d3e0b7f8bc
Zack72-4 <daniedone6@gmail.com>
2026-06-10 08:47:53 -0400

    Mejoras en el checkout y validacion

--- FrontEnd/js/services/finalizar.service.js
+++ FrontEnd/js/services/finalizar.service.js
@@ -1,5 +1,9 @@
 import { apiGet, apiPost } from "../api.js";
 
+export function obtenerCarritoFinalizar() {
+    return apiGet("/carrito/obtener");
+}
+
 export async function finalizarCompra(data) {
     return await apiPost("/finalizar", data);
 }
